bc22bc46ce614d7c4c4c1600c88e5dcd5f3e222c
Keks <keks@htmlacademy.ru>
2022-10-18 23:23:15 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,9 @@
+import './random-array.js';
+import './random-cut-copy.js';
+import {getResult} from './assemble.js';
+import './export-author.js';
+import './export-coordinates.js';
+import './export-offer.js';
+
+// eslint-disable-next-line no-console
+console.log (getResult(10));
